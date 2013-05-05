36bfa381b0662d665ff353d9d1cbcfef736ee947
Artem Titoulenko <artem.titoulenko@gmail.com>
2013-05-05 15:22:17 -0400

    show the albums in a list view

--- src/com/cs314/photoalbum/AlbumActivity.java
+++ src/com/cs314/photoalbum/AlbumActivity.java
@@ -3,7 +3,9 @@ package com.cs314.photoalbum;
 import java.io.File;
 import java.io.FileFilter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Set;
 
 import android.net.Uri;
@@ -17,7 +19,9 @@ import android.graphics.Bitmap;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class AlbumActivity extends Activity {
   Hashtable<String, ArrayList<String>> albums = new Hashtable<String, ArrayList<String>>();
@@ -37,21 +41,27 @@ public class AlbumActivity extends Activity {
       absolutePathOfImage = cursor.getString(column_index);
       albumFilePath = absolutePathOfImage.replaceAll("^(.*)\\/(\\w*\\/.*?\\.(?:jpg|png))$", "$2");
       String[] folderFilePair = albumFilePath.split("/");
-      
+
       if (!folderFilePair[0].isEmpty()) {
         ArrayList<String> album = albums.get(folderFilePair[0]);
         if (album == null) {
           album = new ArrayList<String>();
         }
-        
+
         album.add(absolutePathOfImage);
         albums.put(folderFilePair[0], album);
       }
     }
-    
+
+    ArrayList<String> albumNames = new ArrayList<String>();
     for (String key : albums.keySet()) {
-      System.err.println("album: " + key);
+      albumNames.add(key);
     }
+
+    final ListView listview = (ListView) findViewById(R.id.listview);
+    final StableArrayAdapter adapter = new StableArrayAdapter(this,
+        android.R.layout.simple_list_item_1, albumNames);
+    listview.setAdapter(adapter);
   }
 
   @Override
@@ -59,6 +69,29 @@ public class AlbumActivity extends Activity {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.album, menu);
     return true;
+  }
+
+  private class StableArrayAdapter extends ArrayAdapter<String> {
+
+    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+    public StableArrayAdapter(Context context, int textViewResourceId,
+        List<String> objects) {
+      super(context, textViewResourceId, objects);
+      for (int i = 0; i < objects.size(); ++i) {
+        mIdMap.put(objects.get(i), i);
+      }
+    }
+
+    public long getItemId(int position) {
+      String item = getItem(position);
+      return mIdMap.get(item);
+    }
+
+    public boolean hasStableIds() {
+      return true;
+    }
+
   }
 
 }
